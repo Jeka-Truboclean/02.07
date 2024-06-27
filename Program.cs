01d398b4e79de89b86e7623e968ad6131f66c5bb
Eugen Karaman <karamanjekakol@gmail.com>
2024-06-27 17:20:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace _02._07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now downloading...");
+            Thread thread = new Thread(Download);
+            thread.Start();
+
+            thread.Join();
+            Console.WriteLine("File downloaded!!!");
+        }
+
+        private static void Download()
+        {
+            string url = "https://g.vseigru.net/igra-sonic-adventure.torrent";
+            string filename = "SADX.torrent";
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(url, filename);
+            }
+        }
+    }
+}
